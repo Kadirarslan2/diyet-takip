4b5d36b2ad6bdd6a368b15451a40b34194cc218f
Kadirarslan2 <62059140+Kadirarslan2@users.noreply.github.com>
2026-03-22 15:58:00 +0300

    Update onesignal.js

--- api/onesignal.js
+++ api/onesignal.js
@@ -3,19 +3,20 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).json({error: 'Sadece POST kabul edilir'});
 
     const ONESIGNAL_APP_ID = "ddce2b71-4b68-4437-bc90-7a8f52daca38";
-    
-    // İŞTE SENİN YAKALADIĞIN YEPYENİ, IP ENGELSİZ ŞİFRE!
     const ONESIGNAL_REST_API_KEY = "os_v2_app_3xhcw4klnbcdppeqpkhvfwwkhdzaxfpjq4kespeer22wa3vkpibwfpvzzwbeqccjun3oxwto7qbawjydx46mmp5y5xrwernww3fwdbi"; 
 
     try {
-        const response = await fetch("https://onesignal.com/api/v1/notifications", {
+        // Yeni nesil API adresini kullanıyoruz
+        const response = await fetch("https://api.onesignal.com/notifications", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json; charset=utf-8",
-                "Authorization": `Basic ${ONESIGNAL_REST_API_KEY}`
+                // İŞTE BÜTÜN DÜĞÜM BURADAYDI! "Basic" değil, "Key" olmak ZORUNDA!
+                "Authorization": `Key ${ONESIGNAL_REST_API_KEY}`
             },
             body: JSON.stringify({
                 app_id: ONESIGNAL_APP_ID,
+                target_channel: "push",
                 included_segments: ["Subscribed Users"],
                 headings: req.body.headings,
                 contents: req.body.contents
